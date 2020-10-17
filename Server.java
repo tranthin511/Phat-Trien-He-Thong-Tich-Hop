76920a562ee8f807a45c7a305b220cc989e74d5f
tranthin511 <72056987+tranthin511@users.noreply.github.com>
2020-10-17 20:18:49 +0700

    Bai5
    Bai 5 Deadline 17/10

--- Server.java
+++ Server.java
@@ -1,39 +1,36 @@
+package daochuoi;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintStream;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.net.UnknownHostException;
 import java.util.Scanner;
 public class Server {
 	public static void main(String[] args) {
-
 		try {
 			ServerSocket server = new ServerSocket(7777);
-			System.out.println("Server duoc tao");
+			System.out.println("Server da duoc tao");
 			Socket client = server.accept();
-			System.out.println("Client ket noi den Server");
+			System.out.println("Client da ket noi den server");
 			Scanner inFromClient = new Scanner(client.getInputStream());
 			PrintStream outToClient = new PrintStream(client.getOutputStream());
-			outToClient.println("Ban muon chon sach nao?");
-			String tens = inFromClient.nextLine();
-			System.out.println("client: " + tens);
-			BufferedReader br= new BufferedReader(new FileReader("F:\\sach.txt"));
-			String tep=br.readLine();
-			if(tep.contains(tens))
-				outToClient.println("Co sach " + tens);
-			else
-				outToClient.println("Rat tiec khong co sach " + tens);
-		} catch (IOException e) {
+			outToClient.println("Moi ban nhap mot chuoi:");
+			String a = inFromClient.nextLine();
+			char[] in = a.toCharArray();
+		    int begin=0;
+		    int end=in.length-1;
+		    char temp;
+		    while(end>begin){
+		        temp = in[begin];
+		        in[begin]=in[end];
+		        in[end] = temp;
+		        end--;
+		        begin++;
+		    }
+		    outToClient.println(in);
+			} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}
-		
+			}
 	}
-	
-	
-	
 }
